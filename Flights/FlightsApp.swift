4bb905090d7ff6cb20e43cccb39ee1fd484599fc
alsiraji <m.a.alseraji@gmail.com>
2023-05-30 00:40:30 +0100

    Initial Commit

---
+++ Flights/FlightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightsApp.swift
+//  Flights
+//
+//  Created by Mohammed Alsiraji on 30/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
